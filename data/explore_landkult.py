1792989f31756f69a227c66a746d5be7568c06ff
daniela.herzig <daniela.herzig@students.fhnw.ch>
2023-09-26 14:19:31 +0200

    Flächenanteil- und visualisierung für auszuschliessende Labels, Text

--- data/explore_landkult.py
+++ data/explore_landkult.py
@@ -9,26 +9,31 @@ df_landkult = pd.read_csv('bern_subset_landkult.csv')
 df_landkult.head()
 
 #%%
-"""https://stackoverflow.com/questions/61122875/geopandas-how-to-read-a-csv-and-convert-to-a-geopandas-dataframe-with-polygons"""
-df_landkult['geometry'] = df_landkult['geometry'].apply(wkt.loads)
+labels_zueri = pd.read_csv('labels_zuericrop.csv')
+labels_zueri.head()
 
 #%%
+"""https://stackoverflow.com/questions/61122875/geopandas-how-to-read-a-csv-and-convert-to-a-geopandas-dataframe-with-polygons"""
+df_landkult['geometry'] = df_landkult['geometry'].apply(wkt.loads)
 crs = {'init': 'epsg:2056'}
 gdf = gpd.GeoDataFrame(df_landkult, crs=crs, geometry='geometry')
-
 
 #%%
 gdf.head()
 
 #%%
 gdf.plot(column='LNF_CODE')
+
+#%% [markdown]
+"""
+Der Kanton bern hat eine Fläche von 5'960 km2, die Summe der Agrarflächen, die gemäss Dokumentation im m2 angegeben ist, beträgt:
+"""
 #%%
-a = gdf.LNF_CODE.unique()
+gdf.Shape_Area.sum()
 
 #%%
-labels_zueri = pd.read_csv('labels_zuericrop.csv')
-labels_zueri_array = labels_zueri['LNF_code'].values
-np.isin(a, labels_zueri_array)
+mask = df_landkult['LNF_CODE'].isin(labels_zueri['LNF_code'])
+no_labels = df_landkult[~mask].LNF_CODE.unique()
 
 # %% [markdown]
 """
@@ -44,9 +49,22 @@ Die folgenden Labels kommen nicht in Züricrop vor:
 ## Bäume
 Die 920er Labels sind überlagernde Flächen- und Punktelemente. Das bedeutet, dass diese Nutzungsflächen entweder numerisch oder geometrisch erfasst werden. Die geometrische Nutzung wird als Polygon erfasst. Die numerischen Datenerfassung erfolgt in Kombination mit einem geometrischen Bezug, indem sie als Sachdaten der entsprechden Bewirtschaftungseinheit angehängt werden.
 
+## Permakultur
+Die Permakultur gilt als Spezialkultur. Sie ist Beitragsberechtigt. Wieso diese nicht berücksichtigt wurde im Züricrop, ist reine Hypothese.
+Wir gehen davon aus, dass dies mit der sehr unterschiedlich ausgeprägten Erstellung von Permakulturflächen zusammenhängt.
+
 Aus: Minimale Geodatenmodelle, Landwirtschaftliche Bewirtschaftung, Bezugsjahr 2022, Seite 48-51 Bundesamt für Landwirtschaft
 
 """
 
+#%% 
+# Die Fläche, welche keine für uns verwertbaren Kulturcode-Labels aufweist, beträgt [km2]:
+gdf.Shape_Area.where(gdf['LNF_CODE'].isin(no_labels)).sum()/1000000
 
+# %%
+# Dies entspricht folgendem Flächenprozentsatz des Datensatzes vom Kanton Bern [%]: 
+gdf.Shape_Area.where(gdf['LNF_CODE'].isin(no_labels)).sum()/gdf.Shape_Area.sum()*100
+
+# %%
+gdf.where(gdf['LNF_CODE'].isin(no_labels)).plot(column='LNF_CODE')
 # %%
