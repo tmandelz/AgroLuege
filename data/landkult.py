b7f96f6f4e8c5eea7f3a6c7868d77e31df454acd
Thomas Mandelz <tmandelz@hotmail.com>
2023-09-27 08:22:19 +0200

    add complete landkult dataset

--- data/landkult.py
+++ data/landkult.py
@@ -2,10 +2,10 @@
 import geopandas as gpd
 
 # %%
-x = gpd.read_file("D:/Temp/TorchGEO/LANDKULT/data/LANDKULT_NUTZFL.shp",rows=10000)
+x = gpd.read_file("D:/Temp/TorchGEO/LANDKULT/data/LANDKULT_NUTZFL.shp")
 # %%
 x.columns
-#%%
+# %%
 x["LNF_CODE"].unique()
 # %%
 x.plot()
