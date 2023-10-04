b51ff9d903e98bf6d584b0e45fbc0019ed56a1fa
Thomas Mandelz <tmandelz@hotmail.com>
2023-10-04 15:16:49 +0200

    start eda zuericrop

--- data/zueri_crop.py
+++ data/zueri_crop.py
@@ -3,18 +3,9 @@ import pandas as pd
 import h5py
 #%%
 # Specify the path to the HDF5 file you want to read
-file_path = 'D:\Temp\TorchGEO\ZueriCrop.hdf5'
-
-# Use pandas.read_hdf() to read the HDF5 file into a DataFrame
-df = pd.read_hdf(file_path)
-
-# Now, 'df' contains your data as a DataFrame
-
-# %%
-df
+file_path = r'..\raw_data\ZueriCrop\ZueriCrop.hdf5'
 # %%
 df = h5py.File(file_path, "r", libver='latest', swmr=True)
-# %%
 # %%
 df.keys()
 # %%
