692214da46c4eeed1183e91ab261d25296bf04c3
Thomas Mandelz <tmandelz@hotmail.com>
2023-10-10 17:11:59 +0200

    rewrite data sentinel

--- data/get_data_sentinel.py
+++ data/get_data_sentinel.py
@@ -16,7 +16,76 @@ oauth = OAuth2Session(client=client)
 token = oauth.fetch_token(token_url='https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token',
                           client_secret=client_secret)
 
+#%%
+evalscript = """
+//VERSION=3
+function setup() {
+  return {
+    input: ["B02", "B03", "B04"],
+    mosaicking: Mosaicking.ORBIT,
+    output: { id:"default", bands: 3}
+  }
+}
 
+function updateOutputMetadata(scenes, inputMetadata, outputMetadata) {
+    outputMetadata.userData = { "scenes":  scenes.orbits }
+}
+
+function evaluatePixel(samples) {
+  return [ 2.5 * samples[0].B04, 2.5 * samples[0].B03, 2.5 * samples[0].B02 ]
+}
+"""
+request = {
+    "input": {
+        "bounds": {
+             "bbox": [
+                13.822174072265625,
+                45.85080395917834,
+                14.55963134765625,
+                46.29191774991382
+            ]
+        },
+        "data": [
+            {
+                "type": "sentinel-2-l2a",
+                "dataFilter": {
+                    "timeRange": {
+                        "from": "2022-01-01T00:00:00Z",
+                        "to": "2022-01-31T23:59:59Z"
+                    }
+                }
+            }
+        ]
+    },
+    "output": {
+        "width": 512,
+        "height": 512,
+        "responses": [
+            {
+                "identifier": "default",
+                "format": {
+                    "type": "image/tiff"
+                }
+            },
+            {
+                "identifier": "userdata",
+                "format": {
+                    "type": "application/json"
+                }
+            }
+        ]
+    }
+,    "evalscript": evalscript,
+}
+
+url = "https://sh.dataspace.copernicus.eu/api/v1/process"
+response = oauth.post(url, json=request)
+f = open(f'../data/here_.png', 'wb')
+f.write(response.content)
+f.close()
+
+#%%
+response.content
 # %%
 evalscript = """
 //VERSION=3
@@ -80,4 +149,6 @@ for month in range(1, 13):
     f = open(f'../data/here_{first_day.month}.png', 'wb')
     f.write(response.content)
     f.close()
+# %%
+response
 # %%
