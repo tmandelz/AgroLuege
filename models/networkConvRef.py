92491976c7bfa1f0d17e71ed2c387a4e811adc5b
jan.zwicky <jan.zwicky@students.fhnw.ch>
2023-10-13 10:00:23 +0200

    delete unused 3D convolutions

--- models/networkConvRef.py
+++ models/networkConvRef.py
@@ -21,58 +21,7 @@ def conv3x3(in_planes, out_planes, stride=1, kernel_size=3):
                      bias=False,
                      padding_mode='replicate')
 
-class model_3DConv(torch.nn.Module):
-    def __init__(self, nclasses=36, num_classes_l1=6, num_classes_l2=20, s1_2_s3=None, s2_2_s3=None,
-                 planes=128):
-        super(model_3DConv, self).__init__()
-        self.nclasses = nclasses
-        self.hidden_dim = 2 * nclasses
-        self.planes = planes
 
-        self.s1_2_s3 = s1_2_s3
-        self.s2_2_s3 = s2_2_s3
-
-        self.conv1 = conv3x3x3(self.nclasses , self.planes)
-        self.bn1 = nn.BatchNorm3d(self.planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3x3(self.planes, self.nclasses)
-        self.bn2 = nn.BatchNorm3d(self.nclasses)
-
-    def forward(self, x):
-        x1_, x2_, x3 = x
-        #x1_ = x1_.permute(0, 2, 3, 1)
-        #x2_ = x2_.permute(0, 2, 3, 1)
-        #x3 = x3.permute(0, 2, 3, 1)
-        #x1_ = x1_.contiguous().view(-1, x1_.shape[3])
-        #x2_ = x2_.contiguous().view(-1, x2_.shape[3])
-        #x3 = x3.contiguous().view(-1, x3.shape[3])
-
-        x1 = torch.zeros_like(x3)
-        x2 = torch.zeros_like(x3)
-
-        #padding
-        for i in range(self.s1_2_s3.shape[0]):
-            x1[:, i,:,:] = x1_[:, int(self.s1_2_s3[i]),:,:]
-            x2[:, i,:,:] = x2_[:, int(self.s2_2_s3[i]),:,:]
-
-        x1 = x1.unsqueeze(2)
-        x2 = x2.unsqueeze(2)
-        x3 = x3.unsqueeze(2)
-        x_concat = torch.cat((x1, x2, x3), dim=2)
-
-        out = self.conv1(x_concat)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        #out1 = out[:,:,0,:,:]
-        #out2 = out[:,:,1,:,:]
-        out3 = out[:,:,-1,:,:]
-
-        return F.log_softmax(out3, dim=1)
 
 class model_2DConv(torch.nn.Module):
     def __init__(self, nclasses=36, num_classes_l1=6, num_classes_l2=20, s1_2_s3=None, s2_2_s3=None,
