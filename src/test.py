27812347fd41135a2d69101f89175a3e41705bd3
jan.zwicky <jan.zwicky@students.fhnw.ch>
2023-10-16 15:27:00 +0200

    change weights -> otherwise error

--- src/test.py
+++ src/test.py
@@ -12,20 +12,20 @@ from models.networkConvRef import model_2DConv
 from eval import evaluate_fieldwise
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', "--data", type=str, default='/scratch/tmehmet/train_set_24X24_debug.hdf5', help="path to dataset")
-    parser.add_argument('-fd', "--fold", default=1, type=int, help="5 fold")
-    parser.add_argument('-b', "--batchsize", default=1, type=int, help="batch size")
-    parser.add_argument('-s', "--snapshot",type=str, help="load weights from snapshot",
-                        default='/home/pf/pfstaff/projects/ozgur_MSconvRNN/trained_models_rep/fold1.pth',)
-    parser.add_argument('-hd', "--hidden", default=64, type=int, help="hidden dim")
-    parser.add_argument('-nl', "--layer", default=6, type=int, help="num layer")
-    parser.add_argument('-stg', "--stage", default=3, type=float, help="num stage")
-    parser.add_argument('-id', "--input_dim", default=4, type=int, help="Input channel size")
-    parser.add_argument('-sd', "--seed", default=0, type=int, help="random seed")
-    parser.add_argument('-gt', "--gt_path", default='labels.csv', type=str, help="gt file path")
-    return parser.parse_args()
+# def parse_args():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('-d', "--data", type=str, default='/scratch/tmehmet/train_set_24X24_debug.hdf5', help="path to dataset")
+#     parser.add_argument('-fd', "--fold", default=1, type=int, help="5 fold")
+#     parser.add_argument('-b', "--batchsize", default=1, type=int, help="batch size")
+#     parser.add_argument('-s', "--snapshot",type=str, help="load weights from snapshot",
+#                         default='/home/pf/pfstaff/projects/ozgur_MSconvRNN/trained_models_rep/fold1.pth',)
+#     parser.add_argument('-hd', "--hidden", default=64, type=int, help="hidden dim")
+#     parser.add_argument('-nl', "--layer", default=6, type=int, help="num layer")
+#     parser.add_argument('-stg', "--stage", default=3, type=float, help="num stage")
+#     parser.add_argument('-id', "--input_dim", default=4, type=int, help="Input channel size")
+#     parser.add_argument('-sd', "--seed", default=0, type=int, help="random seed")
+#     parser.add_argument('-gt', "--gt_path", default='labels.csv', type=str, help="gt file path")
+#     return parser.parse_args()
 
 
 def main(
@@ -37,7 +37,8 @@ def main(
         hidden=64,
         stage=3,
         gt_path=None,
-        input_dim=None
+        input_dim=None,
+        num_workers=0,
 ):
 
     testdataset = Dataset(datadir, 0., 'test', eval_mode=True, fold=fold_num, gt_path=gt_path, return_cloud_cover=False,
@@ -77,24 +78,24 @@ def main(
         network.load_state_dict(checkpoint['network_state_dict'])
         network_gt.load_state_dict(checkpoint['network_gt_state_dict'])
 
-    evaluate_fieldwise(network, network_gt, testdataset, batchsize=batchsize, level=1, fold_num=fold_num)
-    evaluate_fieldwise(network, network_gt, testdataset, batchsize=batchsize, level=2, fold_num=fold_num)
-    evaluate_fieldwise(network, network_gt, testdataset, batchsize=batchsize, level=3, fold_num=fold_num)
+    evaluate_fieldwise(network, network_gt, testdataset, batchsize=batchsize, level=1, fold_num=fold_num,workers=num_workers)
+    evaluate_fieldwise(network, network_gt, testdataset, batchsize=batchsize, level=2, fold_num=fold_num,workers=num_workers)
+    evaluate_fieldwise(network, network_gt, testdataset, batchsize=batchsize, level=3, fold_num=fold_num,workers=num_workers)
 
-if __name__ == "__main__":
-    args = parse_args()
-    print(args)
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
+# if __name__ == "__main__":
+#     args = parse_args()
+#     print(args)
+#     np.random.seed(args.seed)
+#     torch.manual_seed(args.seed)
 
-    main(
-        datadir=args.data,
-        fold_num=args.fold,
-        batchsize=args.batchsize,
-        snapshot=args.snapshot,
-        layer=args.layer,
-        hidden=args.hidden,
-        stage=args.stage,
-        gt_path=args.gt_path,
-        input_dim=args.input_dim
-    )
+#     main(
+#         datadir=args.data,
+#         fold_num=args.fold,
+#         batchsize=args.batchsize,
+#         snapshot=args.snapshot,
+#         layer=args.layer,
+#         hidden=args.hidden,
+#         stage=args.stage,
+#         gt_path=args.gt_path,
+#         input_dim=args.input_dim
+#     )
